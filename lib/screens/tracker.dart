1836052c323a3f0d6dc11b9e2c325f749fa673c7
giwi97 <giwanthawijekoon@gmail.com>
2021-04-24 19:36:04 +0530

    initial widget

---
+++ lib/screens/tracker.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Tracker extends StatefulWidget{
+  @override
+  _TrackerState createState() => _TrackerState();
+
+}
+
+class _TrackerState extends State<Tracker> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      
+    );
+  }
+}
