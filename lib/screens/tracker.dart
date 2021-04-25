6fc327e32544a8cbf19929c6ceadef354de7d101
giwi97 <giwanthawijekoon@gmail.com>
2021-04-25 23:17:56 +0530

    minor changes

--- lib/screens/tracker.dart
+++ lib/screens/tracker.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import '../utils/constants.dart';
+
+enum NavigationStatus {
+  GLOBAL,
+  COUNTRY,
+}
 
 class Tracker extends StatefulWidget{
   @override
@@ -10,8 +16,27 @@ class _TrackerState extends State<Tracker> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Container(
-      
+    return Scaffold(
+      resizeToAvoidBottomPadding: false,
+      appBar: AppBar(
+        backgroundColor: kPrimaryColor,
+        elevation: 0,
+        title: Text(
+          "Covid-19 Tracker",
+        ),
+        centerTitle: true,
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.all(32),
+              ),
+            )
+          ],
+        ),
     );
   }
 }
