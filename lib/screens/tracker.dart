70da47f4a1577e34410fcb318dfe2d1c78b7be6f
giwi97 <giwanthawijekoon@gmail.com>
2021-04-26 01:00:06 +0530

    bottom nav bar

--- lib/screens/tracker.dart
+++ lib/screens/tracker.dart
@@ -1,20 +1,20 @@
 import 'package:flutter/material.dart';
 import '../utils/constants.dart';
+import 'navigation_option.dart';
 
 enum NavigationStatus {
   GLOBAL,
   COUNTRY,
 }
 
-class Tracker extends StatefulWidget{
+class Tracker extends StatefulWidget {
   @override
   _TrackerState createState() => _TrackerState();
-
 }
 
 class _TrackerState extends State<Tracker> {
   NavigationStatus navigationStatus = NavigationStatus.GLOBAL;
-  
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -28,36 +28,51 @@ class _TrackerState extends State<Tracker> {
           "Covid-19 Tracker",
         ),
         centerTitle: true,
-        ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-
-            Expanded(
-              child: Container(
-                padding: EdgeInsets.all(32),
-                decoration: BoxDecoration(
-                  color: kPrimaryColor,
-                  borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(50),
-                    bottomRight: Radius.circular(50),
-                  ),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(32),
+              decoration: BoxDecoration(
+                color: kPrimaryColor,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(50),
+                  bottomRight: Radius.circular(50),
                 ),
               ),
             ),
-
-            Container(
-              height: size.height * 0.1,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  
-                ],
-              ),
+          ),
+          
+          Container(
+            height: size.height * 0.1,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                NavigationOption(
+                  title: "Global",
+                  selected: navigationStatus == NavigationStatus.GLOBAL,
+                  onSelected: () {
+                    setState(() {
+                      navigationStatus = NavigationStatus.GLOBAL;
+                    });
+                  },
+                ),
+                NavigationOption(
+                  title: "Country",
+                  selected: navigationStatus == NavigationStatus.COUNTRY,
+                  onSelected: () {
+                    setState(() {
+                      navigationStatus = NavigationStatus.COUNTRY;
+                    });
+                  },
+                )
+              ],
             ),
-
-          ],
-        ),
+          ),
+        ],
+      ),
     );
   }
 }
